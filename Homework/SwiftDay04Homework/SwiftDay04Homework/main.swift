9af5e7fad2c0cc60ed249342dd8ce6fca88abacc
beomjoon8253 <beomjoon@outlook.com>
2025-02-04 22:20:12 +0900

    10일후 날짜 계산

--- Homework/SwiftDay04Homework/SwiftDay04Homework/main.swift
+++ Homework/SwiftDay04Homework/SwiftDay04Homework/main.swift
@@ -47,27 +47,27 @@ for i in 0..<monthDays.count {
 // 1월 1일부터 4월 10일까지의 누적일은 100일입니다.
 
 // 각 월의 날짜수를 저장한 배열.
-let monthDays: [Int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-//               index  0   1   2   3   4   5   6   7   8   9   10  11
-//               month  1   2   3   4   5   6   7   8   9   10  11  12
-var month: Int = 0
-var day: Int = 0
-var total: Int = 0
-var num = 0
-
-// 작업 순서 (의사 코드)
-// 1. 월과 일을 입력 받아서 저장한다.
-// 1-1. 월은 month, 일은 day에 저장.
-// 2. total 변수에 mothDays의 요소값 누적.
-// 2-1. 인덱스가 month - 1보다 작을 때까지 누적하기 (0..<month-1)
-// 3. total에 입력 momth의 day이를 추가.
-
-print("월 입력:", terminator: " ")
-month = Int(readLine() ?? "") ?? 0
-print("일 입력:", terminator: " ")
-day = Int(readLine() ?? "") ?? 0
-
-print("입력 된 날짜는 \(month)월 \(day)일입니다.")
+//let monthDays: [Int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+////               index  0   1   2   3   4   5   6   7   8   9   10  11
+////               month  1   2   3   4   5   6   7   8   9   10  11  12
+//var month: Int = 0
+//var day: Int = 0
+//var total: Int = 0
+//var num = 0
+//
+//// 작업 순서 (의사 코드)
+//// 1. 월과 일을 입력 받아서 저장한다.
+//// 1-1. 월은 month, 일은 day에 저장.
+//// 2. total 변수에 mothDays의 요소값 누적.
+//// 2-1. 인덱스가 month - 1보다 작을 때까지 누적하기 (0..<month-1)
+//// 3. total에 입력 momth의 day이를 추가.
+//
+//print("월 입력:", terminator: " ")
+//month = Int(readLine() ?? "") ?? 0
+//print("일 입력:", terminator: " ")
+//day = Int(readLine() ?? "") ?? 0
+//
+//print("입력 된 날짜는 \(month)월 \(day)일입니다.")
 
 /*
 total = 0 // 누적되는 변수는 초기화 필수.
@@ -124,12 +124,12 @@ while num <= END {
 print(total)
 */
 
-/*
+
 // 과제: Weekly Project는 다음 Project 전까지 계속 진행.
 // 기본과제: 배운것 TIL 정리 ( 배운것 + 알파)
 // 퀴즈: 피보나치 수열 출력, 피보나치 수열에 부호 엇갈라서.
+/*
 // 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 = 54?
-// 1 + 1 - 2 + 3 - 5 + 8 - 13 + 21 = 14?
 let MAX = 21
 var prev = 0
 var cur = 1
@@ -137,24 +137,56 @@ var next = 1
 var flag = false
 total = 0
 while cur <= MAX {
-    
+    // cur 출력
+    print(cur, terminator: cur == MAX ? "=" : "+")
+    total += cur
     // 이전항과 현재항을 더해서 다음 항 만든
     // 현재항은 이전항으로 변경
     // 다음항이 현재항으로 치환 된다.
+    next = prev + cur
+    prev = cur // 덮어 지기 때문에 미리 이전항으로 치환 해 주어야 한다.
+    cur = next
 }
+print(total)
+ */
+
+/*
+// 1 + 1 - 2 + 3 - 5 + 8 - 13 + 21 = 14?
+let MAX = 21
+var prev = 0
+var cur = 1
+var next = 1
+var flag = true
+total = 2
+while cur <= MAX {
+    // 더하기 빼기가 교차 되도록
+    if flag {
+        print(cur, terminator: cur == MAX ? "=" : "+")
+        total -= cur
+    } else {
+        print(cur, terminator: cur == MAX ? "=" : "-")
+        total += cur
+    }
+    flag = !flag
+    
+    next = prev + cur
+    prev = cur
+    cur = next
+}
+print(total)
 */
+
  
 // 내일은 Swift 문법 컬렉션, 함수 진도 나갑니다.
 
-// 문제2) 월,일을 입력 받아서 100일 후의 월,일출력. (난이도: 고)
-// 월 입력: 11월
-// 일 입력: 21
-// 11월 21일의 100일 후는 O월 O일입니다.
-// -----
-// Weekly Project 안내
-// 다음 두가지 중 한가지만 수행 해도 됩니다.
-// 완성한 것은 TIL에 새 페이지를 만들어서 추가 해 주세요.
-// 1) 전화번호부 구현 (입력, 출력, 검색, 수정 삭제 기능)
-// 2) 회고 시스템 CRUD 기능 구현 (개선해서 만드는 것을 권장합니다.)
 
+// 중요: 김춘수 "꽃"
+// 함수나 구조체는 호출하지 않으면 생성되거나 동작 하지 않는다.
+// 불러 줘야 한다!
 
+// 구조체 객체를 생성하고 멤버 호출
+// 생성자 함수 호출
+// exam은 참조변수 (스택 메모리 영역)
+// Example() 생성자 함수를 호출하면 Example의 객체가 힙 메모리 공간에 생성 된다.
+let exam = Example()
+exam.run() // 멤버 메서드 호출(실행)
